446913c5da778fcb0f1a00136451db469dd622c8
Lisa Felder <feldmuesle@yahoo.com>
2021-04-16 19:28:44 +0200

    implented useDataFetch hook

---
+++ src/components/hooks/use-data-fetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { default as restaurants } from "../../data/restaurants.json";
+
+function useDataFetch(url, options) {
+  const [response, setResponse] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setResponse(restaurants);
+      setIsLoading(false);
+    }, 1000);
+
+    /*** If this were an actual api call ***
+
+    fetch(url, options)
+      .then((res) => {
+        setResponse(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setIsLoading(false);
+      });
+    */
+  }, [url]);
+  return [response, isLoading, error];
+}
+
+export default useDataFetch;
