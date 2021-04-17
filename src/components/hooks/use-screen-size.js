d12d6913324fb4b94ee0f53d6966e1b54a50efbe
Lisa Felder <feldmuesle@yahoo.com>
2021-04-17 14:35:56 +0200

    create hook for monitoring screen size

---
+++ src/components/hooks/use-screen-size.js
@@ -0,0 +1,19 @@
+import { useLayoutEffect, useState } from 'react';
+
+function useScreenSize() {
+  const [screenSize, setScreenSize] = useState([0, 0]);
+
+  useLayoutEffect(() => {
+    function updateScreenSize() {
+      setScreenSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateScreenSize);
+    updateScreenSize();
+
+    return () => window.removeEventListener('resize', updateScreenSize);
+  }, []);
+
+  return screenSize;
+}
+
+export default useScreenSize;
